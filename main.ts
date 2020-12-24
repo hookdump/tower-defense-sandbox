05378ef610bf69e82be4fd08f7051374679c64dc
Ignacio <hookdump@gmail.com>
2020-12-23 23:12:49 -0300

    Initial commit

---
+++ main.ts
@@ -0,0 +1,17 @@
+import Phaser from "phaser";
+import HelloWorldScene from "./scenes/HelloWorldScene";
+
+const config: Phaser.Types.Core.GameConfig = {
+  type: Phaser.AUTO,
+  width: 600,
+  height: 800,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 200 }
+    }
+  },
+  scene: [HelloWorldScene]
+};
+
+export default new Phaser.Game(config);
