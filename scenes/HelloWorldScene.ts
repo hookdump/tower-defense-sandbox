4ee836a21ec21220cd6cbcd001dc6ce9db634874
Ignacio <hookdump@gmail.com>
2020-12-23 23:45:19 -0300

    Testing geometry

--- scenes/HelloWorldScene.ts
+++ scenes/HelloWorldScene.ts
@@ -6,23 +6,28 @@ export default class HelloWorldScene extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image("sky", "assets/skies/space3.png");
-    this.load.image("logo", "assets/sprites/phaser3-logo.png");
-    this.load.image("red", "assets/particles/red.png");
+    this.load.image("ship", "assets/sprites/ship.png");
+    this.load.image("tower", "assets/sprites/tower.png");
+  }
+
+  renderUI() {
+    const graphics = this.add.graphics();
+    graphics.lineStyle(4, 0x2ecc40);
+    graphics.strokeLineShape(new Phaser.Geom.Line(0, 400, 600, 400));
   }
 
   create() {
-    this.add.image(400, 300, "sky");
+    // this.add.image(400, 300, "wood");
+    this.renderUI();
 
-    const particles = this.add.particles("red");
-
+    const particles = this.add.particles("tower");
     const emitter = particles.createEmitter({
       speed: 100,
       scale: { start: 1, end: 0 },
       blendMode: "ADD"
     });
 
-    const logo = this.physics.add.image(400, 100, "logo");
+    const logo = this.physics.add.image(400, 100, "ship");
 
     logo.setVelocity(100, 200);
     logo.setBounce(1, 1);
