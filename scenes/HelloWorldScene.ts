fef9ddfffc2852b4a1c326df5bf9cd63dc1d70c2
Ignacio <hookdump@gmail.com>
2020-12-24 01:02:43 -0300

    Pathfinding prep

--- scenes/HelloWorldScene.ts
+++ scenes/HelloWorldScene.ts
@@ -1,9 +1,11 @@
 import Phaser, { Game } from "phaser";
+import EasyStar from "easystarjs";
 import { TILE_SIZE, SCREEN_H } from "../constants";
 
 export default class HelloWorldScene extends Phaser.Scene {
   map: Phaser.Tilemaps.Tilemap;
   marker: Phaser.GameObjects.Graphics;
+  finder: EasyStar.js;
 
   constructor() {
     super("hello-world");
@@ -16,7 +18,8 @@ export default class HelloWorldScene extends Phaser.Scene {
     this.load.image("tower", "assets/sprites/tower.png");
   }
 
-  renderUI(graphics) {
+  renderUI() {
+    const graphics = this.add.graphics();
     const lineY = (TILE_SIZE * SCREEN_H) / 2;
     const splitLine = new Phaser.Geom.Line(0, lineY, 600, lineY);
     graphics.lineStyle(4, 0x2ecc40);
@@ -26,13 +29,13 @@ export default class HelloWorldScene extends Phaser.Scene {
   create() {
     // this.add.image(400, 300, "background");
 
-    const graphics = this.add.graphics();
-    this.renderUI(graphics);
-
     // Display map
     this.map = this.make.tilemap({ key: "map" });
     const tiles = this.map.addTilesetImage("tiles", "tileset");
-    this.map.createStaticLayer(0, tiles, 0, 0);
+    const tileLayer = this.map.createStaticLayer(0, tiles, 0, 0);
+    // tileLayer.setCollisionByExclusion([-1, 20], true);
+
+    this.renderUI();
 
     // Marker following the mouse
     this.marker = this.add.graphics();
@@ -48,11 +51,41 @@ export default class HelloWorldScene extends Phaser.Scene {
     });
     */
 
-    const ship = this.physics.add.image(400, 100, "ship");
+    // Path finder
+    this.finder = new EasyStar.js();
 
+    // Create 2D representation of map for pathfinder
+    const grid = [];
+    for (let y = 0; y < this.map.height; y++) {
+      let col = [];
+      for (let x = 0; x < this.map.width; x++) {
+        const tileId = tileLayer.getTileAt(x, y).index;
+        col.push(tileId);
+      }
+      grid.push(col);
+    }
+    this.finder.setGrid(grid);
+
+    // Set up walkable tiles
+    const tileset = this.map.tilesets[0];
+    const properties = tileset.tileProperties;
+    const walkableTiles = [];
+
+    for (let i = tileset.firstgid - 1; i < tiles.total; i++) {
+      if (!properties.hasOwnProperty(i)) {
+        // No property, it is walkable
+        walkableTiles.push(i + 1);
+        continue;
+      }
+      if (!properties[i].collide) walkableTiles.push(i + 1);
+    }
+    this.finder.setAcceptableTiles(walkableTiles);
+
+    const ship = this.physics.add.image(400, 100, "ship");
     ship.setVelocity(0, 200);
     ship.setBounce(1, 1);
     ship.setCollideWorldBounds(true);
+    this.physics.add.collider(ship, tileLayer);
 
     // emitter.startFollow(ship);
   }
